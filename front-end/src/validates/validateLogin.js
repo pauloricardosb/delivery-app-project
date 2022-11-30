b63b1f9423300316073360727be82af321ee80be
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-11-30 11:30:26 -0300

    Requisito 3 e 8 - Validação de Login e Cadastro

--- front-end/src/validates/validateLogin.js
+++ front-end/src/validates/validateLogin.js
@@ -1,20 +1,13 @@
-export const validateLogin = (email, password) => {
-  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+const MIN_LENGTH_NAME = 12;
+const MIN_LENGTH_PASSWORD = 6;
+const EMAIL_REGEX = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-  if (!email.match(emailRegex)) {
-    return false;
-  };
-  if (password.length < 6) {
-    return false;
-  }
-  return true;
-}
+export const validateLogin = (email, password) => (
+  EMAIL_REGEX.test(email) && password.length >= MIN_LENGTH_PASSWORD
+);
 
 export const validateUser = (name, email, password) => {
   const validate = validateLogin(email, password);
 
-  if(!validate || name.length < 12) {
-    return false;
-  }
-  return true;
-}
+  return validate && name.length >= MIN_LENGTH_NAME;
+};
