ecc85c4e99b58cbb50a56aa8b366fdb126adcf8b
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-12-01 13:47:33 -0300

    Feat: create token middleware

---
+++ back-end/src/api/middlewares/token.middleware.js
@@ -0,0 +1,14 @@
+const { validateToken } = require("../helpers/jwt");
+
+const validationToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+  const user = validateToken(authorization);
+  req.user = { ...user };
+  next();
+
+}
+
+module.exports = validationToken;
