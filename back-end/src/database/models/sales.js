37562cdeff6eea3b541ab5c068ee147171fe3f9a
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-12-02 15:53:03 -0300

    FIX: Remove obsolete code

--- back-end/src/database/models/sales.js
+++ back-end/src/database/models/sales.js
@@ -21,17 +21,6 @@ module.exports = (sequelize, DataTypes) => {
         underscored: true,
     });
 
-    // Sale.associate = (models) => {
-    //     Sale.belongsTo(models.User, {
-    //         foreignKey: 'id', as: 'user_id'
-    //     });
-    // }
-    // Sale.associate = (models) => {
-    //     Sale.belongsTo(models.User, {
-    //         foreignKey: 'sellerId', as: 'seller_id'
-    //     })
-    // }
-
     Sale.associate = (models) => {
         Sale.belongsTo(models.User, {
             foreignKey: 'user_id', as: 'user'
