daa38e335d0e1c271f70b275f1408dc130cf3e3b
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-12-02 15:51:10 -0300

    FEAT: configure Products and Orders route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const loginRouter = require('./routes/login.routes');
 const registerRouter = require('./routes/register.routes');
 const productsRouter = require('./routes/products.routes');
+const ordersRouter = require('./routes/orders.routes');
 
 const app = express();
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
 app.use('/login', loginRouter); 
 app.use('/register', registerRouter);
-app.use('/customer', productsRouter)
+app.use('/costumer/products', productsRouter);
+app.use('/costumer/orders', ordersRouter);
 
 module.exports = app;
