6ca62892d2792060f420c6c697e45d7269a480e9
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-12-02 15:53:29 -0300

    FIX: rename files

--- back-end/src/api/services/sales.service.js
+++
@@ -1,8 +0,0 @@
-const { SaleProduct } = require('../../database/models');
-
-const getOrderById = async (saleId) => {
-  const order = await SaleProduct.findOne({ where: { saleId } });
-  return { id: saleId };
-}
-
-module.exports = { getOrderById }
