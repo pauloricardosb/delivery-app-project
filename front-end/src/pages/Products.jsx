19079f679ba7ae876adafe7f0e99108743c62374
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-12-06 10:36:35 -0300

    FEAT: Cards dos produtos

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/NavbarComponent';
+import ProductCard from '../components/ProductCard';
 
 function Products() {
   return (
     <div>
       <Navbar />
+      <ProductCard />
     </div>
   );
 }
