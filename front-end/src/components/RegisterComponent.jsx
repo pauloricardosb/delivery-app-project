45e8e6cd9255764d1c0d4f0a57c6df6aa57f1bb0
Airtonsilv4 <airton.silva19@hotmail.com>
2022-12-06 21:50:51 -0300

    FEAT: Validator Register

--- front-end/src/components/RegisterComponent.jsx
+++ front-end/src/components/RegisterComponent.jsx
@@ -1,7 +1,22 @@
 import { useState } from 'react';
+import validator from 'validator';
 
 function RegisterComponent() {
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    password: '',
+  });
   const [failedTryRegister] = useState(false);
+
+  function isButtonDisabled() {
+    const { nome, email, password } = formData;
+    const MIN_CHARACTERS_NAME = 12;
+    const MIN_CHARACTERS_PASS = 6;
+    return (password.length < MIN_CHARACTERS_PASS)
+      || (nome.length < MIN_CHARACTERS_NAME)
+      || (!validator.isEmail(email));
+  }
 
   return (
     <div>
@@ -11,6 +26,13 @@ function RegisterComponent() {
           type="text"
           id="common_register__input-name"
           data-testid="common_register__input-name"
+          value={ formData.nome }
+          onChange={
+            (e) => setFormData((current) => ({
+              ...current,
+              nome: e.target.value,
+            }))
+          }
         />
       </label>
       <label htmlFor="common_register__input-email">
@@ -19,6 +41,13 @@ function RegisterComponent() {
           type="text"
           id="common_register__input-email"
           data-testid="common_register__input-email"
+          value={ formData.email }
+          onChange={
+            (e) => setFormData((current) => ({
+              ...current,
+              email: e.target.value,
+            }))
+          }
         />
       </label>
       <label htmlFor="common_register__input-password">
@@ -27,11 +56,19 @@ function RegisterComponent() {
           type="password"
           id="common_register__input-password"
           data-testid="common_register__input-password"
+          value={ formData.password }
+          onChange={
+            (e) => setFormData((current) => ({
+              ...current,
+              password: e.target.value,
+            }))
+          }
         />
       </label>
       <button
         type="button"
         data-testid="common_register__button-register"
+        disabled={ isButtonDisabled() }
       >
         CADASTRAR
       </button>
