ddfe48c264c99a284d9b06c378393f40e1a3e77b
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-12-13 15:32:05 -0300

    FEAT: Checkout redirect

--- front-end/src/components/CartShop.jsx
+++ front-end/src/components/CartShop.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { localCart } from '../helpers/localStorage';
 
 function CartShop() {
@@ -18,12 +19,21 @@ function CartShop() {
 
   return (
     <div>
-      <button
-        type="button"
-        data-testid="customer_products__checkout-bottom-value"
+      <Link
+        to="/customer/checkout"
       >
-        { (cart.length > 0) ? subTotal() : '0,00' }
-      </button>
+        <button
+          type="button"
+          data-testid="customer_products__button-cart"
+          disabled={ cart.length === 0 }
+        >
+          <p
+            data-testid="customer_products__checkout-bottom-value"
+          >
+            { (cart.length > 0) ? subTotal() : '0,00' }
+          </p>
+        </button>
+      </Link>
     </div>
   );
 }
