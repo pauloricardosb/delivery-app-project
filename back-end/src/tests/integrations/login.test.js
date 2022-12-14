ef23e48528f2d75712a2868dce0ff3e5e56e92e3
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-12-14 17:23:42 -0300

    FEAT: Register tests

--- back-end/src/tests/integrations/login.test.js
+++ back-end/src/tests/integrations/login.test.js
@@ -10,6 +10,11 @@ const {
   loginResponse,
   validLogin,
   dbResponse,
+  existentUser,
+  existentUserMessage,
+  newUser,
+  userCreated,
+  registerResponse,
 } = require("../mocks/login.mocks");
 
 const { expect } = chai;
@@ -37,6 +42,34 @@ describe("Login", () => {
 
     expect(response).to.have.status(200);
     expect(response.body).to.be.deep.equal(loginResponse);
+
+    sinon.restore();
+  });
+});
+
+describe("Register", () => {
+  it("should return 409 if user email already registered", async () => {
+    sinon.stub(User, "findOne").resolves(dbResponse);
+
+    const response = await chai
+      .request(app)
+      .post("/register")
+      .send(existentUser);
+
+    expect(response).to.have.status(409);
+    expect(response.body).to.be.deep.equal(existentUserMessage);
+
+    sinon.restore();
+  });
+
+  it("should return 201 when register a new user", async () => {
+    sinon.stub(User, "findOne").resolves();
+    sinon.stub(User, "create").resolves(userCreated);
+
+    const response = await chai.request(app).post("/register").send(newUser);
+
+    expect(response).to.have.status(201);
+    expect(response.body).to.be.deep.equal(registerResponse);
 
     sinon.restore();
   });
--- back-end/src/tests/mocks/login.mocks.js
+++ back-end/src/tests/mocks/login.mocks.js
@@ -23,4 +23,52 @@ const loginResponse = {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRGVsaXZlcnkgQXBwIEFkbWluIiwiZW1haWwiOiJhZG1AZGVsaXZlcnlhcHAuY29tIiwicm9sZSI6ImFkbWluaXN0cmF0b3IifQ.YGYHTyrRLZ-7L4dQAvJKbRVg3Racs61EIGaXdBZ1iBI",
 };
 
-module.exports = { validLogin, invalidLogin, dbResponse, loginResponse };
+const existentUser = {
+  name: "Delivery App Admin",
+  email: "adm@deliveryapp.com",
+  password: "adminPassword",
+};
+
+const newUser = {
+  name: "Gabriel Faustino",
+  email: "faustino@email.com",
+  password: "batata123",
+};
+
+const userCreated = {
+  dataValues: {
+    id: 4,
+    name: "Gabriel Faustino",
+    role: "customer",
+    email: "faustino@email.com",
+    password: "aca3ec7278a47144c0a863e60c595abe",
+  },
+};
+
+const existentUserMessage = { message: "Email already registered" };
+
+const registerResponse = {
+  user: {
+    name: "Gabriel Faustino",
+    email: "faustino@email.com",
+    role: "customer",
+  },
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiR2FicmllbCBGYXVzdGlubyIsImVtYWlsIjoiZmF1c3Rpbm9AZW1haWwuY29tIiwicm9sZSI6ImN1c3RvbWVyIn0.85HOdiS2IZtrQsJP3oiE7bA02atSMCFKrYnp2eSLNko",
+};
+
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRGVsaXZlcnkgQXBwIEFkbWluIiwiZW1haWwiOiJhZG1AZGVsaXZlcnlhcHAuY29tIiwicm9sZSI6ImFkbWluaXN0cmF0b3IifQ.YGYHTyrRLZ-7L4dQAvJKbRVg3Racs61EIGaXdBZ1iBI";
+
+module.exports = {
+  validLogin,
+  invalidLogin,
+  dbResponse,
+  loginResponse,
+  existentUser,
+  existentUserMessage,
+  newUser,
+  userCreated,
+  registerResponse,
+  token,
+};
